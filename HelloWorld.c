d80da910671e9535dcde1018d3803cdb727e813c
Subha <subhadeeppal2004@gmail.com>
2022-05-30 12:17:43 +0530

    initial commit

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b,c;
+    printf("enter the number you want to square\n");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("your square value is %d",c); 
+    return 0;
+}
